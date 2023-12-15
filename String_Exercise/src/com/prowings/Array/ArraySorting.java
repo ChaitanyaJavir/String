f466e695dead06c2813e7c6da170874769b28c1d
ChaitanyaJavir <chaitanyajavir26@gmail.com>
2023-12-15 15:56:49 +0530

    Arrays

--- String_Exercise/src/com/prowings/Array/ArraySorting.java
+++ String_Exercise/src/com/prowings/Array/ArraySorting.java
@@ -1,5 +1,16 @@
 package com.prowings.Array;
 
-public class ArraySorting {
+import java.util.Arrays;
 
+public class ArraySorting {
+		public static void main(String[] args) {
+			
+			int[] array = {2,5,4,3,1};
+			Arrays.sort(array);
+			
+			System.err.println("Sorting of Array: " + Arrays.toString(array));
+			
+			int index = Arrays.binarySearch(array, 4);
+			System.out.println("Binaray of array: " +index);
+		}
 }
